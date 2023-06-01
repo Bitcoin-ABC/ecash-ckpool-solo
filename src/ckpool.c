eb8225ceda9fcff7fd8522de1701d615bfd1f6c3
Con Kolivas <kernel@kolivas.org>
2023-06-01 10:07:59 +1000

    Copyright

--- src/ckpool.c
+++ src/ckpool.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2020 Con Kolivas
+ * Copyright 2014-2020,2023 Con Kolivas
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
